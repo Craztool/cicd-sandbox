21645e5052531e408716db5dd9e5a27c4d6748a1
Craztool <ineeddewey@gmail.com>
2026-06-18 13:10:07 +0700

    kub learn start

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-def add(a, b):
-    return a + b
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
+class SimpleServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        html = "<html><body><h1>Привет от CI/CD Sandbox V2!</h1><p>Секретарь Ingress успешно пропустил нас в Kubernetes!</p></body></html>"
+        self.wfile.write(html.encode('utf-8'))
 
 if __name__ == "__main__":
-    print(add(2, 3))
+    port = int(os.environ.get("PORT", 8000))
+    print(f"Сервер запущен на порту {port}")
+    server = HTTPServer(("", port), SimpleServer)
+    server.serve_forever()
